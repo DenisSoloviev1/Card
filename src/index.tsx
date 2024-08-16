45c3bc06b5f433996ae3a51c3ac75db577d3114a
Denis <solovievdenis2004@gmail.com>
2024-08-16 13:20:57 +0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./App/App.tsx";
+import App from "./app/App.tsx";
 
 const rootElement: HTMLElement | null = document.getElementById("root");
 
