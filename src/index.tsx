f26a1791c8187a7fe038f10580d672a0156c134d
Denis <solovievdenis2004@gmail.com>
2024-08-16 13:24:36 +0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
-import App from "./App/App.tsx";
+import App from "./app/App.tsx";
 
 const rootElement: HTMLElement | null = document.getElementById("root");
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(<App />);
 } else {
-  console.error("Root element not found");
+  console.error("Root element not found :(");
 }
